9296ad72b8993d0ea970ab134a2117475faccbde
weiguang wang <wangweiguang1@gmail.com>
2017-10-19 16:22:52 -0500

    config

---
+++ PetsChatServer/config/config.js
@@ -0,0 +1,18 @@
+'use strict'
+
+module.exports = {
+  qiniu: {
+    video: 'http://video.iblack7.com/',
+    AK: 'xxx',
+    SK: 'xxx'
+  },
+  cloudinary: {
+    cloud_name: 'gougou',
+    api_key: '852224485571877',
+    api_secret: 'xxx',
+    base: 'http://res.cloudinary.com/gougou',
+    image: 'https://api.cloudinary.com/v1_1/gougou/image/upload',
+    video: 'https://api.cloudinary.com/v1_1/gougou/video/upload',
+    audio: 'https://api.cloudinary.com/v1_1/gougou/raw/upload'
+  }
+}
